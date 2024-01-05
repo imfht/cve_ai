3fc6e061d63d7dc95475cddc40144e36e909a128
fangjinxu <f.jinxu@gmail.com>
2024-01-05 09:16:33 +0800

    add f1 score

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import pandas as pd
 import sklearn
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score, accuracy_score
+
 
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
@@ -56,16 +58,18 @@ def build_index():
         df = set_label(df, columns)
     return df
 
+def f1_multiclass(labels, preds):
+    return f1_score(labels, preds, average='micro')
 
 def do_learn(df, label_column, base_model='roberta', sub_model='roberta-base'):
     from simpletransformers.classification import ClassificationModel, ClassificationArgs
     new_df = pd.DataFrame()
     new_df['text'] = df['desc']
     new_df['labels'] = df[label_column]
-    train_df, test_df = train_test_split(new_df)
+    train_df, test_df = train_test_split(new_df,test_size=0.1)
     train_df = pd.DataFrame(train_df)
     # train_df.columns = ["text", "labels"]
-    model_args = ClassificationArgs(num_train_epochs=1, output_dir=f'./test_{label_column}_{base_model}_{sub_model}')
+    model_args = ClassificationArgs(num_train_epochs=5, output_dir=f'./test_{label_column}_{base_model}_{sub_model}')
     model_args.use_multiprocessing = False
     model_args.early_stopping_delta = 0.05
     model_args.early_stopping_metric = "mcc"
@@ -80,9 +84,10 @@ def do_learn(df, label_column, base_model='roberta', sub_model='roberta-base'):
         base_model, sub_model, args=model_args, num_labels=len(new_df['labels'].unique()), use_cuda=True
     )
     model.train_model(train_df)
-
-    result = model.eval_model(test_df, acc=sklearn.metrics.accuracy_score)
-    print(result[0])
+    result, predict, wrong = model.eval_model(test_df, acc=sklearn.metrics.accuracy_score, f1=f1_multiclass)
+    print("total: %s, wrong: %s"%(len(test_df),len(wrong)))
+    
+    print(result)
 
 
 if __name__ == '__main__':
